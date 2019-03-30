4320169828b07d9a56deeaf57cbb1b4e20712be7
Mattias Sämskar <mattias.samskar@gmail.com>
2019-03-30 16:31:59 +0100

    Remove unused Express config

--- server.js
+++ server.js
@@ -8,39 +8,35 @@ var app = express();
 var router = express.Router();
 
 app.use(express.static("build"));
-var port = process.env.API_PORT || 3001;
 //now we should configure the API to use bodyParser and look for
 //JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //now we can set the route path & initialize the API
-router.get("/", function(req, res) {
-  res.json({ message: "API Initialized!" });
-});
+// router.get("/", function(req, res) {
+//   res.json({ message: "API Initialized!" });
+// });
 //Use our router configuration when we call /api
-app.use("/api", router);
+// app.use("/api", router);
 //starts the server and listens for requests
-app.listen(port, function() {
-  console.log(`api running on port ${port}`);
-});
 
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
+// app.use(function(req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   );
 
-  //and remove cacheing so we get the most recent comments
-  res.setHeader("Cache-Control", "no-cache");
-  next();
-});
+//   //and remove cacheing so we get the most recent comments
+//   res.setHeader("Cache-Control", "no-cache");
+//   next();
+// });
 
 // db ======================
 //mongoose.connect('mongodb://localhost/midsummer');              // connect to mongoDB database
@@ -138,4 +134,10 @@ app.delete("/api/participant/:scoreId", function(req, res) {
     if (err) res.status(400).end();
     res.send();
   });
+});
+
+var port = process.env.API_PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`api running on port ${port}`);
 });
